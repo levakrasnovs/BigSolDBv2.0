64212460ef1ba0e1cb927b6f20b995e59e76151b
Лев Краснов <levkrasnov@MacBook-Pro-Lev-2.local>
2025-03-28 13:57:12 +0300

    add new version

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,9 +102,16 @@ with tabs[1]:
     selected = st.selectbox(label='Choose molecule', options=compound_names, index=None, placeholder='Paracetamol')
     if selected:
         search_df = df[(df['Compound_Name'] == selected)]
+        search_df.reset_index(drop=True, inplace=True)
+        col1result, col2result = st.columns([1, 1])
+        pubchem = search_df['PubChem_CID'].iloc[0]
+        cas = search_df['CAS'].iloc[0]
+        if pubchem is not None:
+            col1result.markdown(f'PubChem link: **https://pubchem.ncbi.nlm.nih.gov/compound/{pubchem}**')
+        if cas is not None:
+            col2result.markdown(f'CAS link: **https://commonchemistry.cas.org/detail?cas_rn={cas}**')
+            
         canonize_mol = search_df['SMILES_Solute'].iloc[0]
-
-        st.markdown(f'### This compound was found in BigSolDB 2.0:')
         col1result, col2result, col3result = st.columns([1, 1, 2])
         col1result.markdown(f'**Molecule from BigSolDB 2.0**')
         col2result.markdown(f'**Source**')
