e1e9e1580ab344f2782639349765a22bd6ce061a
Лев Краснов <levkrasnov@MacBook-Pro-Lev-2.local>
2025-03-28 13:39:41 +0300

    add new version

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,23 +100,24 @@ with tabs[0]:
 with tabs[1]:
 
     selected = st.selectbox(label='Choose molecule', options=compound_names, index=None, placeholder='Paracetamol')
-    search_df = df[(df['Compound_Name'] == selected)]
-    canonize_mol = search_df['SMILES_Solute'].iloc[0]
+    if selected:
+        search_df = df[(df['Compound_Name'] == selected)]
+        canonize_mol = search_df['SMILES_Solute'].iloc[0]
 
-    st.markdown(f'### This compound was found in BigSolDB 2.0:')
-    col1result, col2result, col3result = st.columns([1, 1, 2])
-    col1result.markdown(f'**Molecule from BigSolDB 2.0**')
-    col2result.markdown(f'**Source**')
-    col3result.markdown(f'**Solubility**')
-    dois = list(search_df['Source'].unique())
-    for doi in dois:
+        st.markdown(f'### This compound was found in BigSolDB 2.0:')
         col1result, col2result, col3result = st.columns([1, 1, 2])
-        df_comp = search_df[(search_df['Source'] == doi) & (search_df['SMILES_Solute'] == canonize_mol)]
-        fig_line = px.line(df_comp, x="Temperature_K", y="Solubility(mole_fraction)", color="Solvent", title=f"Dependence of solubility on temperature", markers=True)
-        fig_line.update_layout(yaxis_title='Solubility (mole fraction)')
-        col1result.image(draw_molecule(canonize_mol), caption=canonize_mol)
-        col2result.markdown(f'**https://doi.org/{doi}**')
-        col3result.plotly_chart(fig_line)
+        col1result.markdown(f'**Molecule from BigSolDB 2.0**')
+        col2result.markdown(f'**Source**')
+        col3result.markdown(f'**Solubility**')
+        dois = list(search_df['Source'].unique())
+        for doi in dois:
+            col1result, col2result, col3result = st.columns([1, 1, 2])
+            df_comp = search_df[(search_df['Source'] == doi) & (search_df['SMILES_Solute'] == canonize_mol)]
+            fig_line = px.line(df_comp, x="Temperature_K", y="Solubility(mole_fraction)", color="Solvent", title=f"Dependence of solubility on temperature", markers=True)
+            fig_line.update_layout(yaxis_title='Solubility (mole fraction)')
+            col1result.image(draw_molecule(canonize_mol), caption=canonize_mol)
+            col2result.markdown(f'**https://doi.org/{doi}**')
+            col3result.plotly_chart(fig_line)
 
 with tabs[2]:
 
