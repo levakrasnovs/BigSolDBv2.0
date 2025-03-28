5410f2b74a584e517e43511f999c7f765b309553
Лев Краснов <levkrasnov@MacBook-Pro-Lev-2.local>
2025-03-28 14:13:21 +0300

    add new version

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,7 @@ st.set_page_config(page_title='BigSolDBv2.0', layout="wide")
 df = pd.read_csv('BigSolDBv2.0.csv')
 df['PubChem_CID'] = df['PubChem_CID'].astype('Int64')
 compound_names = sorted(df['Compound_Name'].unique().tolist())
+fda_compound_names = sorted(df[df['FDA_Approved'] == 'Yes']]['Compound_Name'].unique().tolist())
 
 df_smiles = pd.DataFrame({'SMILES_Solute': list(df['SMILES_Solute'].unique())})
 df_smiles['mol'] = df_smiles['SMILES_Solute'].apply(Chem.MolFromSmiles)
@@ -99,34 +100,38 @@ with tabs[0]:
     st.plotly_chart(fig_hist, use_container_width=True)
 
 with tabs[1]:
+    fda = st.checkbox("Only FDA Approved molecules")
 
-    selected = st.selectbox(label='Choose molecule', options=compound_names, index=None, placeholder='Paracetamol')
-    if selected:
-        search_df = df[(df['Compound_Name'] == selected)]
-        search_df.reset_index(drop=True, inplace=True)
-        col1result, col2result = st.columns([1, 1])
-        pubchem = search_df['PubChem_CID'].iloc[0]
-        cas = search_df['CAS'].iloc[0]
-        if pubchem is not None:
-            col1result.markdown(f'PubChem link: **https://pubchem.ncbi.nlm.nih.gov/compound/{pubchem}**')
-        if cas is not None:
-            col2result.markdown(f'CAS link: **https://commonchemistry.cas.org/detail?cas_rn={cas}**')
+    if fda:
+        selected = st.selectbox(label='Choose molecule', options=fda_compound_names, index=None, placeholder='Paracetamol')
+    else:
+        selected = st.selectbox(label='Choose molecule', options=compound_names, index=None, placeholder='Paracetamol')
+        if selected:
+            search_df = df[(df['Compound_Name'] == selected)]
+            search_df.reset_index(drop=True, inplace=True)
+            col1result, col2result = st.columns([1, 1])
+            pubchem = search_df['PubChem_CID'].iloc[0]
+            cas = search_df['CAS'].iloc[0]
+            if pubchem is not None:
+                col1result.markdown(f'PubChem link: **https://pubchem.ncbi.nlm.nih.gov/compound/{pubchem}**')
+            if cas is not None:
+                col2result.markdown(f'CAS link: **https://commonchemistry.cas.org/detail?cas_rn={cas}**')
 
-        canonize_mol = search_df['SMILES_Solute'].iloc[0]
-        col1result, col2result, col3result = st.columns([1, 1, 2])
-        col1result.markdown(f'**Molecule from BigSolDB 2.0**')
-        col2result.markdown(f'**Source**')
-        col3result.markdown(f'**Solubility**')
-        dois = list(search_df['Source'].unique())
-        for doi in dois:
+            canonize_mol = search_df['SMILES_Solute'].iloc[0]
             col1result, col2result, col3result = st.columns([1, 1, 2])
-            df_comp = search_df[(search_df['Source'] == doi) & (search_df['SMILES_Solute'] == canonize_mol)]
-            fig_line = px.line(df_comp, x="Temperature_K", y="Solubility(mole_fraction)", color="Solvent", title=f"Dependence of solubility on temperature", markers=True)
-            fig_line.update_layout(yaxis_title='Solubility (mole fraction)')
-            col1result.image(draw_molecule(canonize_mol), caption=canonize_mol)
-            col2result.markdown(f'**https://doi.org/{doi}**')
-            col3result.plotly_chart(fig_line)
-        st.dataframe(search_df)
+            col1result.markdown(f'**Molecule from BigSolDB 2.0**')
+            col2result.markdown(f'**Source**')
+            col3result.markdown(f'**Solubility**')
+            dois = list(search_df['Source'].unique())
+            for doi in dois:
+                col1result, col2result, col3result = st.columns([1, 1, 2])
+                df_comp = search_df[(search_df['Source'] == doi) & (search_df['SMILES_Solute'] == canonize_mol)]
+                fig_line = px.line(df_comp, x="Temperature_K", y="Solubility(mole_fraction)", color="Solvent", title=f"Dependence of solubility on temperature", markers=True)
+                fig_line.update_layout(yaxis_title='Solubility (mole fraction)')
+                col1result.image(draw_molecule(canonize_mol), caption=canonize_mol)
+                col2result.markdown(f'**https://doi.org/{doi}**')
+                col3result.plotly_chart(fig_line)
+            st.dataframe(search_df)
 
 with tabs[2]:
 
